5f34589b0f14553ab72dcedee074e5ca99281b5d
jialmanzal <jesusalmanza@gmail.com>
2020-10-29 05:47:26 -0500

    Update routes.py

--- resources/routes.py
+++ resources/routes.py
@@ -2,5 +2,5 @@ from .client import ClientsApi, ClientApi
 
 
 def initialize_routes(api):
-    api.add_resource(ClientsApi, '/api/clients')
-    api.add_resource(ClientApi, '/api/client/<id>')
+    api.add_resource(ClientsApi, '/api/consultarestadocuenta')
+    
